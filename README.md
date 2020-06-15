3562e286199390235dde3104e701e8d3cfd35840
Rob Taylor <manix84@gmail.com>
2020-06-15 08:17:06 +0100

    Initial project division from gmod_discord_bot project

--- README.md
+++ README.md
@@ -1,1 +1,72 @@
-# discord_gmod_bot
+# TTT Discord Bot ![Icon](https://raw.githubusercontent.com/manix84/ttt_discord_bot/master/images/icon/icon_64x.png)
+
+>Dead players tell no tales!
+
+*... and that's basically what this bot does.*
+
+[![price](https://img.shields.io/badge/price-free-brightgreen.svg)](LICENSE)
+[![gmod-addon](https://img.shields.io/badge/gmod-addon-_.svg?colorB=1194EF)](https://wiki.garrysmod.com)
+[![discord-bot](https://img.shields.io/badge/discord-bot-_.svg?colorB=8C9EFF)](https://discord.js.org)
+[![license](https://img.shields.io/github/license/marceltransier/ttt_discord_bot.svg)](LICENSE)
+
+This is a powerful [discord bot](https://discord.js.org) that mutes dead players in [TTT](http://ttt.badking.net) (Garry's Mod - Trouble in Terrorist Town)
+
+## Getting Started
+If you need a step-by-step tutorial, follow my [guide at steam](http://steamcommunity.com/sharedfiles/filedetails/?id=1351369388)
+
+### Prerequisites
+- You have to have already installed a Garry's Mod Server with the TTT Gamemode.
+- You must have a [Nodejs](https://nodejs.org) server (I used [Heroku.com](https://heroku.com))
+
+### Installation
+1. Clone this repository (to e.g. your home folder) and install the requirements
+     ```bash
+     cd ~
+     git clone https://github.com/manix84/ttt_discord_bot.git
+     cd ttt_discord_bot
+     npm install --prefix ./discord_bot/
+     ```
+2. Rename the `config.json.example` in `config.json`
+
+3. Create Discord Bot, invite him to your server and paste the token in the config
+
+   - if you don't know how to, follow [this guide](https://github.com/reactiflux/discord-irc/wiki/Creating-a-discord-bot-&-getting-a-token)
+
+   - insert the bot token at `discord -> token` in the config.json
+   
+   - grant the bot the permissions to mute members
+
+4. Insert the Guild *(Server)* id and the channel id in the config
+
+   - if you don't know how to get these id's, follow [this guide](https://support.discordapp.com/hc/en-us/articles/206346498-Where-can-I-find-my-User-Server-Message-ID-)
+
+   - insert the guild id at `discord -> guild` and the cannel id of the voice channel in wich the bot should mute dead players at `discord -> channel` in the config.json
+
+5. Add the addon to the garrysmod server
+
+   - Move the `gmod_addon` folder to garrysmod/addons and name it suitable e.g. `ttt_discord_bot`
+
+   - Or add the [workshop object]() to  the servers collection
+
+### Usage
+
+- Start the bot by runing the node command with the `ttt_discord_bot/discord_bot/` directory
+- Connect your Steam Account with the bot by typing `!discord YourDiscordTag` in the ingame chat. E.g `!discord Manix84`. If you're lazy and there is nobody else with your discord discriminator (`#4402`) in the ttt channel, it's enough if you use this. E.g. `!discord 4402`
+- If you're in the **configured voice channel**, the game state is **in progress**, you're **connected with discord** and you die in TTT, the bot will mute you!
+
+## Credits
+
+- Marcel Transier - The original creator of [ttt_discord_bot](https://github.com/marceltransier/ttt_discord_bot.git)
+- I used [discord.js](https://discord.js.org) in this project. Thanks for the easy opportunity writing a discord bot in javascript!
+- Thanks for the great Garry's Mod gamemode [Trouble in Terrorist Town](http://ttt.badking.net) I made this bot for.
+
+## Contributing
+
+1. Fork it (<https://github.com/marceltransier/ttt_discord_bot/fork>)
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Commit your changes (`git commit -am 'Add some fooBar'`)
+4. Push to the branch (`git push origin feature/fooBar`)
+5. Create a new Pull Request
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ web.js
@@ -0,0 +1,190 @@
+const Discord = require('discord.js');
+// const config = require('./config.json') || {
+const config = {
+  discord: {},
+  server: {},
+  keepAlive: {}
+};
+const log = console.log;
+const http = require('http');
+const https = require('https');
+
+const DISCORD_GUILD = config.discord.guild || process.env.DISCORD_GUILD;
+const DISCORD_CHANNEL = config.discord.channel || process.env.DISCORD_CHANNEL;
+const HOST = config.server.host || process.env.HOST || 'localhost';
+const PORT = config.server.port || process.env.PORT || 37405; //unused port and since now the OFFICIAL ttt_discord_bot port ;)
+const KEEPALIVE_HOST = config.keepAlive.host || process.env.KEEPALIVE_HOST || HOST;
+const KEEPALIVE_PORT = config.keepAlive.port || process.env.KEEPALIVE_PORT || PORT;
+const KEEPALIVE_ENABLED = config.keepAlive.enabled || process.env.KEEPALIVE_ENABLED || false;
+
+log('Constants: ');
+log("  DISCORD_GUILD: ", DISCORD_GUILD, `(${typeof DISCORD_GUILD})`);
+log("  DISCORD_CHANNEL: ", DISCORD_CHANNEL, `(${typeof DISCORD_CHANNEL})`);
+log("  HOST: ", HOST, `(${typeof HOST})`);
+log("  PORT: ", PORT, `(${typeof PORT})`);
+log("  KEEPALIVE_HOST: ", KEEPALIVE_HOST, `(${typeof KEEPALIVE_HOST})`);
+log("  KEEPALIVE_PORT: ", KEEPALIVE_PORT, `(${typeof KEEPALIVE_PORT})`);
+log("  KEEPALIVE_ENABLED: ", KEEPALIVE_ENABLED, `(${typeof KEEPALIVE_ENABLED})`);
+
+let guild;
+let channel;
+
+const muted = {};
+
+const get = [];
+
+//create discord client
+const client = new Discord.Client();
+client.login(process.env.DISCORD_TOKEN);
+
+client.on('ready', () => {
+  log('Bot is ready to mute them all! :)');
+  guild = client.guilds.get(DISCORD_GUILD);
+  // guild = client.guilds.find('id', DISCORD_GUILD);
+  channel = guild.channels.get(DISCORD_CHANNEL);
+  // channel = guild.channels.find('id', DISCORD_CHANNEL);
+});
+client.on('voiceStateUpdate', (oldMember, newMember) => { //player leaves the ttt-channel
+  if (oldMember.voiceChannel != newMember.voiceChannel && isMemberInVoiceChannel(oldMember)) {
+    if (isMemberMutedByBot(newMember) && newMember.serverMute) newMember.setMute(false).then(() => {
+      setMemberMutedByBot(newMember, false);
+    });
+  }
+});
+
+isMemberInVoiceChannel = (member) => member.voiceChannelID == DISCORD_CHANNEL;
+isMemberMutedByBot = (member) => muted[member] == true;
+setMemberMutedByBot = (member, set = true) => muted[member] = set;
+
+get['connect'] = (params, ret) => {
+  let tag_utf8 = params.tag.split(" ");
+  let tag = "";
+
+  tag_utf8.forEach((e) => {
+    tag = tag + String.fromCharCode(e);
+  });
+
+  let found = guild.members.filterArray(val => val.user.tag.match(new RegExp('.*' + tag + '.*')));
+  if (found.length > 1) {
+    ret({
+      answer: 1 //pls specify
+    });
+  } else if (found.length < 1) {
+    ret({
+      answer: 0 //no found
+    });
+  } else {
+    ret({
+      tag: found[0].user.tag,
+      id: found[0].id
+    });
+  }
+};
+
+get['mute'] = (params, ret) => {
+  let id = params.id;
+  let mute = params.mute;
+  if (typeof id !== 'string' || typeof mute !== 'boolean') {
+    ret();
+    return;
+  }
+  log("Muted: " + id);
+  //let member = guild.members.find('id', id);
+  let member = guild.members.find(user => user.id === id);
+
+  if (member) {
+
+    if (isMemberInVoiceChannel(member)) {
+      if (!member.serverMute && mute) {
+        member.setMute(true, "dead players can't talk!").then(() => {
+          setMemberMutedByBot(member);
+          ret({
+            success: true
+          });
+        }).catch((err) => {
+          ret({
+            success: false,
+            error: err
+          });
+        });
+      }
+      if (member.serverMute && !mute) {
+        member.setMute(false).then(() => {
+          setMemberMutedByBot(member, false);
+          ret({
+            success: true
+          });
+        }).catch((err) => {
+          ret({
+            success: false,
+            error: err
+          });
+        });
+      }
+    } else {
+      ret();
+    }
+
+  } else {
+    ret({
+      success: false,
+      err: 'member not found!' //TODO lua: remove from ids table + file
+    });
+  }
+};
+
+get['keep_alive'] = (params, ret) => {
+  ret({
+    success: true,
+  });
+};
+
+const keepAliveReq = () => {
+  const url = `https://${KEEPALIVE_HOST}:${KEEPALIVE_PORT}/?req=keep_alive`;
+  const options = {
+//     host: KEEPALIVE_HOST,
+//     port: KEEPALIVE_PORT,
+//     path: '/?req=keep_alive',
+    method: 'GET',
+    timeout: 5 * 1000 // 5 second request timeout.
+  };
+  log('[KeepAlive] Requesting:', url, 'with options:', options);
+  https.request(url, options, (res) => {
+    res.on('data', (chunk) => {
+      try {
+        log('[KeepAlive] Success!');
+        log('[KeepAlive] Response: ' + chunk);// disable after it's working
+      } catch (err) {
+        log(err.message);
+      }
+    });
+  }).on('timeout', (e) => {
+  }).on('error', (err) => {
+    log('[KeepAlive] Error Message:', err.message);
+    log('[KeepAlive] Error:', err)
+  });
+};
+
+
+http.createServer((req, res) => {
+  if (typeof req.headers.params === 'string' && typeof req.headers.req === 'string' && typeof get[req.headers.req] === 'function') {
+    try {
+      let params = JSON.parse(req.headers.params);
+      get[req.headers.req](params, (ret) => res.end(JSON.stringify(ret)));
+    } catch (e) {
+      res.end('no valid JSON in params');
+    }
+  } else
+    res.end();
+}).listen({
+  port: PORT
+}, () => {
+  log(`Bot endpoint is running: https://${KEEPALIVE_HOST}:${KEEPALIVE_PORT}`);
+
+  if (KEEPALIVE_ENABLED) {
+    log(`[KeepAlive] Starting.`);
+
+    setInterval(keepAliveReq, 20 * 60 * 1000); // load every 20 minutes
+    setTimeout(keepAliveReq, 10 * 1000); // load first attempt after 10 seconds.
+  }
+});
